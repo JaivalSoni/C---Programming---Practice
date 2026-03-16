cd0a86eabba49517e85f9be880088ba41e247485
Jaival Soni <98264975+JaivalSoni@users.noreply.github.com>
2026-03-16 16:46:47 +0530

    Implement sum function to add two integers

--- Chapter 6/Problems/04_problem.c
+++ Chapter 6/Problems/04_problem.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
-    int main(){
 
-    printf("");
+int sum(int *, int *);
+
+int sum(int *a, int *b){
+    *a = 7;
+    return *a + *b;
+}
+
+int main(){
+    int x = 5,y = 5;
+    printf("Enter the sum of x and y is %d",sum(&x,&y));
 
     return 0;
 }
